b11c434b41558b7ce7a7e348ed955513c49c8f28
Colin Vaughan <colinmvaughan@gmail.com>
2017-07-08 12:40:56 -0400

    Squashed 'ECS/' content from commit 0748edc
    git-subtree-dir: ECS
    git-subtree-split: 0748edc9bc46ef05bebab7d309d31d365e4cdfab

---
+++ ECS/Main.cpp
@@ -0,0 +1,22 @@
+//#include "ComponentManager.h"
+//#include "SystemManager.h"
+//#include "BaseSystem.h"
+//
+//int main()
+//{
+//	ComponentManager CManager;
+//	CManager.AddComponent<int>(0);
+//	CManager.AddComponent<bool>(0);
+//
+//	CManager.AddComponent<int>(1);
+//	CManager.AddComponent<int>(2);
+//
+//	
+//	if (CManager.HasComponents<int, bool, float>(0))
+//		std::cout << "Has required Component(s)";
+//	else
+//		std::cout << "Does not have required Component(s)";
+//
+//
+//	return 0;
+//}

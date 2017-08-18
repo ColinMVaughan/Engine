02e735ab2bcb39860d6d0631effa242f472c0ede
Colin Vaughan <colinmvaughan@gmail.com>
2017-08-17 21:03:23 -0400

    Moved PhysicSystem code from header to source file.

--- Physics/Include/PhysicsSystem.h
+++ Physics/Include/PhysicsSystem.h
@@ -2,93 +2,37 @@
 #define PHYSICS_SYSTEM_H
 
 #include "PxPhysicsAPI.h"
-
-
 using namespace physx;
+
+
+class RigidActor
+{
+public:
+	PxRigidActor* m_RigidActor;
+};
+
+class RigidBody
+{
+public:
+	PxRigidBody* m_RigidBody;
+};
+
+
+
 class PhysicsSystem
 {
 public:
 
 
-	bool Initalize()
-	{	
-		//Create Foundation for Physics
-		m_Foundation = PxCreateFoundation(PX_FOUNDATION_VERSION, m_defaultAllocator, m_defaultErrorCallback);
-		if (!m_Foundation)
-			return false;
+	bool Initalize();
+	void Unload();
 
-		//Set Up PVD for connectin to external physics debugger
-		m_Pvd = PxCreatePvd(*m_Foundation);
-		PxPvdTransport* transport = PxDefaultPvdSocketTransportCreate("127.0.0.1", 5425, 10);
-		m_Pvd->connect(*transport, PxPvdInstrumentationFlag::eALL);
+	PxScene* CreateScene(PxSceneDesc a_SceneDescription);
+	PxPhysics* GetPhysics();
+	PxScene* GetScene();
 
-		//Create physics class
-		m_Physics = PxCreatePhysics(PX_PHYSICS_VERSION, *m_Foundation, PxTolerancesScale(), true, m_Pvd);
-		if (!m_Physics)
-			return false;
+	void StepPhysics();
 
-		m_Dispatcher = PxDefaultCpuDispatcherCreate(2);
-
-		PxSceneDesc defaultSceneDescription(m_Physics->getTolerancesScale());
-		defaultSceneDescription.gravity = PxVec3(0.0f, -9.81f, 0.0f);
-
-		m_Scene = CreateScene(defaultSceneDescription);
-	}
-
-	void Unload()
-	{
-		PX_UNUSED(false);
-		m_Scene->release();
-		m_Pvd->release();
-		m_Physics->release();
-		m_Foundation->release();
-	}
-	//---------------------------------------------------------------------------------------------------
-	//	CreateScene() Description:
-	//	Generates Scene from description, fills in some boilerplate info, and sets up the scene for PVD
-	//
-	//	Params: 
-	//	- a_SceneDescription: settings for the scene that will be created
-	//
-	//	Returns:
-	//	-Pointer to the newly created scene. User owns pointer, responsible for deletion. 
-	//----------------------------------------------------------------------------------------------------
-	PxScene* CreateScene(PxSceneDesc a_SceneDescription)
-	{
-
-		a_SceneDescription.cpuDispatcher = m_Dispatcher;
-		a_SceneDescription.filterShader = PxDefaultSimulationFilterShader;
-
-		PxScene* tempScene = m_Physics->createScene(a_SceneDescription);
-
-		PxPvdSceneClient* pvdClient = tempScene->getScenePvdClient();
-		if (pvdClient)
-		{
-			pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONSTRAINTS, true);
-			pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_CONTACTS, true);
-			pvdClient->setScenePvdFlag(PxPvdSceneFlag::eTRANSMIT_SCENEQUERIES, true);
-		}
-
-		return tempScene;
-	}
-
-	PxPhysics* GetPhysics()
-	{
-		return m_Physics;
-	}
-
-	PxScene* GetScene()
-	{
-		return m_Scene;
-	}
-
-	void StepPhysics()
-	{
-		PX_UNUSED(false); //I don't know what this does
-		m_Scene->simulate(1.0f / 20.0f);
-		m_Scene->fetchResults(true);
-
-	}
 private:
 
 
@@ -104,8 +48,6 @@ private:
 	PxMaterial* m_DefaultMaterial;
 	PxPvd* m_Pvd;
 
-	
 };
-
 
 #endif

6e7221c56e74447c7e31dfd42977612a3749743c
Colin Vaughan <colinmvaughan@gmail.com>
2018-01-12 14:04:50 -0500

    Added some notes to Asset Manager and started editor integration.

--- Engine/Application/Include/AssetManager.h
+++ Engine/Application/Include/AssetManager.h
@@ -1,10 +1,25 @@
 #ifndef ASSET_MANAGER_H
 #define ASSET_MANAGER_H
 
+//----------------------------------------------------------------------------
+//					Notes to Self
+// *The Asset Manager needs to be reworked
+//		-How do we differentiate project assets from scene assets?
+//		-How should users add assets to the scene?
+//		-The asset manager should probably be seperate from all UI, right?
+//
+//	*Asset Access/Storage/Retreaval should probably be hooked into Events...
+//		-We should get Events up and running before we worry about assets.
+//----------------------------------------------------------------------------
+
+
+#include <imgui.h>
+
 #include <thread>
 #include <vector>
 #include <string>
 #include <map>
+#include <tuple>
 
 class BaseAssetPool
 {
@@ -18,8 +33,8 @@ public:
 //					Asset Pool
 //
 // Holds a vector of asset type T.
-//
-// 
+// Has Ability to Load, store and retrieve asset of type T.
+// Is used in combination with the editor.
 //---------------------------------------------------
 
 template<typename T>
@@ -33,16 +48,27 @@ public:
 		//m_Pool.push_back(T(filePath));
 	}
 
+
+	virtual void DisplayInEditor()
+	{
+		//Display all Assets of type T in the editor
+		for (auto it = m_Pool.begin(); it < m_Pool.end(); ++it)
+		{
+			//display selectable with name given from first element of tuple
+			ImGui::Selectable(std::get<0>(it*), nullptr);
+		}
+	}
+
 private:
-	std::vector<T> m_Pool;
+	std::vector<std::tuple<std::string, T>> m_Pool;
+
 };
 
 
-//------------------------------------------------------------------
 class AssetManager
 {
 public:
-
+	
 	template<typename T>
 	void AddResourceType(std::string typeName)
 	{
@@ -55,9 +81,17 @@ public:
 		}
 	}
 
+	//Display the AssetManager window. Allowing the user to view and add different 
+	
+	void DisplayInEditor()
+	{
+		ImGui::SetNextWindowSize(ImVec2(500, 440), ImGuiCond_FirstUseEver);
+	}
+
 private:
 	std::thread m_WorkerThread;
 	std::map<std::string, BaseAssetPool*> m_PoolMap;
+	bool IsWindowActive = false;
 };
 
 #endif

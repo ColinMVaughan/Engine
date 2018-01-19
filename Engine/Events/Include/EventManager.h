36d3a32e7bfac7d6ccf8d2b8287bb42b516d1ebb
Colin Vaughan <colinmvaughan@gmail.com>
2018-01-19 11:21:05 -0500

    Finished bare bones of EventManager.

--- Engine/Events/Include/EventManager.h
+++ Engine/Events/Include/EventManager.h
@@ -1,35 +1,72 @@
 #ifndef EVENT_MANAGER_H
 #define EVENT_MANAGER_H
 
+#include <functional>
+#include <vector>
+#include <map>
+
+
+//Event Interface: All other events must inherit from IEvent.
+class IEvent
+{
+public:
+	IEvent();
+	virtual ~IEvent();
+
+	int ID = 0;
+private:
+};
+
+	
 //----------------------------------------------
 //				Event Manager
 //				Colin Vaughan
 //
-//	
+// 
 //
 //----------------------------------------------
-
-class IEvent
+class EventManager
 {
-	
-};
+public:
+	using FunctionType = std::function<void(const IEvent&)>;
 
+	template<typename EventType>
+	void AddListner(EventType a_event, FunctionType&& a_func);
 
-class EventQueue
-{
-	static void init()
-	{
-		m_head = 0;
-		m_tail = 0;
-	}
-	void DispatchEvent(IEvent* a_event);
-	void RegisterListner();
+	void RemoveListner();
+
+	template<typename EventType>
+	void DispatchEvent(EventType& a_event);
 
 private:
-	static int m_head;
-	static int m_tail;
-
-	IEvent m_Queue[100];
+	std::map<size_t, std::vector<FunctionType>> Listners;
 };
+
+
+
+//----------------------------------------------
+//			IMPLEMENTATION
+//----------------------------------------------
+
+template<typename EventType>
+void EventManager::AddListner(EventType a_event, FunctionType&& a_func)
+{
+	auto ID = typeid(EventType).hash_code();
+	Listners[ID].push_back(a_func);
+}
+
+
+template<typename EventType>
+void EventManager::DispatchEvent(EventType& a_event)
+{
+
+	int type = a_event.ID;
+	if (Listners.find(type) == Listners.end())
+		return;
+
+	auto&& ListnerList = Listners.at(type);
+	for (auto&& listner : ListnerList)
+		listner(a_event);
+}
 
 #endif
--- Engine/Events/Src/EventManager.cpp
+++ Engine/Events/Src/EventManager.cpp
@@ -1,2 +1,22 @@
 #include "EventManager.h"
 
+//void EventManager::AddListner(int a_event, FunctionType&& a_func)
+//{
+//	Listners[a_event].push_back(a_func);
+//}
+
+void EventManager::RemoveListner()
+{
+
+}
+
+//void EventManager::DispatchEvent(int a_entity, IEvent& a_event)
+//{
+//	int type = a_event.ID;
+//	if (Listners.find(type) == Listners.end())
+//		return;
+//
+//	auto&& ListnerList = Listners.at(type);
+//	for (auto&& listner : ListnerList)
+//		listner(a_event);
+//}

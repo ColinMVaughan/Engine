8e07d9d1ecf5b25bee1d1696434f65ef88bc30d1
Colin Vaughan <colinmvaughan@gmail.com>
2018-01-25 11:54:14 -0500

    Addded events tests

---
+++ Testing/Test_Events/Include/EventTest.h
@@ -0,0 +1,70 @@
+#ifndef _EVENTS_TESTING_H_
+#define _EVENTS_TESTING_H_
+
+#include <gtest\gtest.h>
+#include <EventManager.h>
+#include <string>
+
+class TestEvent : public IEvent
+{
+public:
+	std::string Message;
+};
+
+class TestEvent2 : public IEvent
+{
+public:
+
+};
+
+class TestEventHandler
+{
+public:
+	void HandleEvent(const TestEvent& a_event)
+	{
+		
+	}
+
+	bool EventRecieved = false;
+};
+
+//--------------------------------------------------------------
+//							TESTS
+//--------------------------------------------------------------
+
+//Test that a listner can be added to the event manager
+TEST(EventManagerTest, AddListnerTest)
+{
+	TestEventHandler handler;
+
+	EventManager manager;
+	manager.AddListner<TestEvent>((std::bind(&TestEventHandler::HandleEvent, handler, std::placeholders::_1)));
+
+	TestEvent eve;
+	manager.DispatchEvent<TestEvent>(eve);
+
+	EXPECT_TRUE(handler.EventRecieved);
+}
+
+//Test that events only trigger listeners registered to it
+TEST(EventManagerTest, EventDispatch)
+{
+	TestEventHandler handler;
+	EventManager manager;
+	manager.AddListner<TestEvent>(std::bind(&TestEventHandler::HandleEvent, handler, std::placeholders::_1));
+	
+	manager.DispatchEvent<TestEvent2>(TestEvent2());
+	EXPECT_FALSE(handler.EventRecieved);
+
+	manager.DispatchEvent<TestEvent>(TestEvent());
+	EXPECT_TRUE(handler.EventRecieved);
+
+}
+
+//Test that Events are cleaned up after dispatching to lisners.
+TEST(EventManagerTest, EventMemoryManagement)
+{
+
+}
+
+#endif

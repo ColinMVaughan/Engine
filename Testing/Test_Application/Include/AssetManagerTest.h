d3c78418b6df70b5969736f06fd9ccab8918884b
Colin Vaughan <colinmvaughan@gmail.com>
2018-02-04 19:56:55 -0500

    Added tests for Application

---
+++ Testing/Test_Application/Include/AssetManagerTest.h
@@ -0,0 +1,23 @@
+#ifndef _ASSET_MANAGER_TESTS_H_
+#define _ASSET_MANAGER_TESTS_H_
+
+#include <gtest\gtest.h>
+#include <AssetManager.h>
+
+
+TEST(AssetManagerTest, AddResourceType)
+{
+	AssetManager manager;
+	manager.AddResourceType
+}
+
+TEST(AssetManagerTest, RetrieveAsset)
+{
+
+}
+
+TEST(AssetManagerTest, RemoveAsset)
+{
+
+}
+#endif

aa7628e00bbbd1b69dc3e7b3ad0a4b41a1f5351d
Colin Vaughan <colinmvaughan@gmail.com>
2018-04-09 19:09:08 -0400

    Forgot to add BaseSystem source file, adding now.

---
+++ Engine/ECS/Src/BaseSystem.cpp
@@ -0,0 +1,32 @@
+#include <BaseSystem.h>
+#include <ECS.h>
+
+
+void ECS::BaseSystem::RegisterEntity(Entity& a_Entity)
+{
+	//check that entity has required components
+	if (!HasComponents(a_Entity.GetID()))
+		return;
+
+	//check that entity is not already registered
+	for each(Entity registeredEnt in m_RegisteredEntities)
+	{
+		if (a_Entity.GetID() == registeredEnt.GetID())
+			return;
+	}
+
+	//add entity to vector and call user defined registration funciton
+	m_RegisteredEntities.push_back(a_Entity);
+	EntityRegistered(a_Entity);
+}
+
+
+void ECS::BaseSystem::UpdateSystem(double deltaTime)
+{
+	PreUpdate(deltaTime);
+	for each(Entity entity in m_RegisteredEntities)
+	{
+		Update(deltaTime, entity);
+	}
+	PostUpdate(deltaTime);
+}

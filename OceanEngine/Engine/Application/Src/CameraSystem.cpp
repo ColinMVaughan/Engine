59077d81cfc6cf70ac0b675ea331f4fe54894e56
Colin Vaughan <colinmvaughan@gmail.com>
2018-08-13 16:05:38 -0400

    Cleaned up old code.

--- OceanEngine/Engine/Application/Src/CameraSystem.cpp
+++ OceanEngine/Engine/Application/Src/CameraSystem.cpp
@@ -7,9 +7,6 @@ DebugCameraControlSystem::DebugCameraControlSystem(ECS::ComponentManager* a_cman
 	rotations = PxVec2(0);
 	m_MotionType = MOTION_NONE;
 
-	//REGISTER_EVENT_LISTNER(KeyPressedEvent, MyKeyDown, a_eManager)
-	//REGISTER_EVENT_LISTNER(KeyReleasedEvent, MyKeyUp, a_eManager)
-	//REGISTER_EVENT_LISTNER(MouseMovedEvent, MyMouseMoved, a_eManager)
 
 	RegisterEventListner<KeyPressedEvent>(a_eManager, [this](KeyPressedEvent& eve) {MyKeyDown(eve); });
 	//RegisterEventListner<KeyReleasedEvent>(a_eManager, [this](KeyReleasedEvent& eve) {MyKeyUp(eve); });
@@ -29,93 +26,40 @@ void DebugCameraControlSystem::Update(double deltaTime, ECS::Entity& entity)
 {
 	//Get Transform and camera components of entity
 	auto transform = entity.GetComponent<Transform>();
-	//auto camera = entity.GetComponent<Camera>();
 
-	//Rotate camera by whatever ammout
-	//transform->GetTransform()->q = PxQuat(PxIdentity);
-	//transform->GetTransform()->q *= PxQuat(rotations.x, PxVec3(0, 1, 0));
-	//transform->GetTransform()->q *= PxQuat(rotations.y, PxVec3(1, 0, 0));
-
-	//PxVec3 zoom(0, 0, Zoom);
-
-	//zoom = transform->GetTransform()->q.rotate(zoom);
-
-	//Translate camera in orientation corrected movement direction
-	//transform->GetTransform()->p = zoom;//+= transform->GetTransform()->rotate(MoveDirection) * deltaTime;
-
-
-	//---------------------------------------------------
+	//initalize matricies as identity
 	PxMat44 Zoom = PxMat44(physx::PxIdentity);
 	PxMat44 Rot = PxMat44(physx::PxIdentity);
 	PxMat44 Translate = PxMat44(physx::PxIdentity);
 
 	PxQuat quatRot = PxQuat(rotations.x, PxVec3(0, 1, 0)) * PxQuat(rotations.y, PxVec3(1, 0, 0));
 
-
+	//Set up position, rotation, and zoom matricies
 	Zoom.setPosition(PxVec3(0.0f, 0.0f, MyZoom));
 	Rot = PxMat44(quatRot);
 	Translate.setPosition(MoveDirection);
 
 	PxMat44 finalMat = Translate * Rot * Zoom;
-	//finalMat = Translate * finalMat;
 
-	//*transform->GetTransform() = PxTransform(Translate);
+	//give the transform component the final position and rotation
 	transform->GetTransform()->p = finalMat.transform(PxVec3(0));
 	transform->GetTransform()->q = quatRot;
 
-	//--------------------------------------------------
-
-
-	// Convert physx::PxMat44 to glm::mat4 and send updated transform to camera component
-	// (I would like to change this in the fututre)
-	//camera->m_Transform = glm::make_mat4(transform->GetGlobalTransformMatrix().front());
 	return;
 }
 
 void DebugCameraControlSystem::PostUpdate(double deltaTime)
 {
-	//zero out rotation for next update
-	//rotation = PxQuat(PxIDENTITY::PxIdentity);
-
-	//zero out move direction for next update
-	//MoveDirection *= 0;
-
-
 }
 
 
 //Checks that the relivent modifier key has been pressed.
 void DebugCameraControlSystem::MyKeyDown(KeyPressedEvent& key)
 {
-	float Speed = 0.5f;
-
-	switch (key.InputEvent.keysym.sym)
-	{
-	case 'W':
-		MoveDirection.z -= Speed;
-		break;
-	case 'A':
-		MoveDirection.x -= Speed;
-		break;
-	case 'S':
-		MoveDirection.z += Speed;
-		break;
-	case'D':
-		MoveDirection.x += Speed;
-		break;
-	case 'E':
-		MoveDirection.y += Speed;
-		break;
-	case 'Q':
-		MoveDirection.y -= Speed;
-		break;
-
-	}
 }
 
 void DebugCameraControlSystem::MyKeyUp(KeyReleasedEvent& key)
 {
-
 }
 
 // If the Mouse is moved, Rotate the Camera's View Matrix 
